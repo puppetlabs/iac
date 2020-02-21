4ba70fec6421cd7d3ef02ebba900f4b429924953
DavidS <DavidS@users.noreply.github.com>
2020-02-21 13:57:37 +0000

    Update github pages

--- bin/kerminator_to_blog_md.sh
+++ bin/kerminator_to_blog_md.sh
@@ -6,7 +6,7 @@ SUPPORTED_TAG="[supported]"
 
 print_usage() {
 	echo -e "\nUSAGE:\n"
-	echo "Run kerminator in Slack to get the last 14 days of released modules: !modules released 14."
+	echo "Run kerminator in Slack to get the last 7 days of released modules: !modules released 7."
 	echo "Save the output of kerminator to a file."
 	echo "Run this script with the first arg pointing to the file above."
 	echo -e "\n./kerminator_to_blog_md.sh /path/to/kerminator_output.txt\n"
@@ -27,11 +27,10 @@ while read line; do
 		version=$(echo -ne "v"; echo -ne $line | cut -d ' ' -f 3)
 		output="$output\n- [$moduleName]($FORGE_BASE_URI/$moduleName) ($version)"
 	else
-		skippedModules=$(($skippedModules + 1))	
+		skippedModules=$(($skippedModules + 1))
 	fi
 done < "$1"
 
 echo -e $output | sort
 echo ""
 echo "Skipped unsupported modules: $skippedModules"
-
